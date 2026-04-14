3863fa0c616afe6ded0dff59ef82f47831774fe7
abdulmalikbarbhuiya882-cmd <abdulmalikbarbhuiya882@gmail.com>
2026-04-14 08:32:41 +0530

    Create tracker.py

---
+++ tracker.py
@@ -0,0 +1,24 @@
+import requests
+import time
+from datetime import datetime
+
+API_KEY = "AIzaSyDZzf2gSAqvmq_1fTUlL7YCVZ2LCjvSvh4"
+VIDEO_ID = "R6ddyz2AhbA"
+
+last_views = None
+
+while True:
+    try:
+        url = f"https://www.googleapis.com/youtube/v3/videos?part=statistics&id={VIDEO_ID}&key={API_KEY}"
+        r = requests.get(url).json()
+
+        views = r['items'][0]['statistics']['viewCount']
+
+        if views != last_views:
+            print(datetime.now().strftime("%H:%M"), "Views:", views)
+            last_views = views
+
+    except:
+        print("Error fetching")
+
+    time.sleep(300)
