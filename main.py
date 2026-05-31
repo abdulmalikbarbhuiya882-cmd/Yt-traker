ae47beffe2017da900c471cbccf00cb4d1859941
abdulmalikbarbhuiya882-cmd <abdulmalikbarbhuiya882@gmail.com>
2026-05-31 10:47:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 from datetime import datetime
 
 API_KEY = "AIzaSyDZzf2gSAqvmq_1fTUlL7YCVZ2LCjvSvh4"
-VIDEO_ID = "3KiFyEq4JFY"
+VIDEO_ID = "LC54zrzU3oQ"
 last_views = None
 
 while True:
